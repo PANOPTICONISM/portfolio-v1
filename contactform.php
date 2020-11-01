3086cc2f547c61ac10245562d5c83c50885cba2e
PANOPTICONISM <mariaalmeidc@hotmail.com>
2020-11-01 21:13:55 +0100

    recaptcha

--- contactform.php
+++ contactform.php
@@ -10,7 +10,23 @@
         $headers = "From: ".$mailFrom;
         $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-        mail($mailTo, $subject, $txt, $headers);
-        header("Location: index.php?mailsend");
+        // mail($mailTo, $subject, $txt, $headers);
+
+        $secretKey = "6LcH590ZAAAAABT2FYcnBeBcYZodQEpSWcNdQnDt";
+        $responseKey = $_POST['g-recaptcha-response'];
+        $UserIP = $_SERVER['REMOTE_ADDR'];
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$UserIP";
+
+        $response = file_get_contents($url);
+        $response = json_decode($response);
+
+        if ($response->success) {
+            mail($mailTo, $subject, $txt, $headers);
+            echo "Message sent successfully";
+        } else {
+            echo "Invalid Captcha. Please, try again.";
+        }
+
+        // header("Location: index.php?mailsend");
     }
 ?>
