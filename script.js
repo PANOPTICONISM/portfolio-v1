83772502b84fc4c4c7431247e35170b42471a3c0
panopticonism <mariaalmeidc@hotmail.com>
2021-04-01 11:34:01 +0200

    fade-in + appear-in animations added

--- script.js
+++ script.js
@@ -160,3 +160,31 @@ for (var i = 0; i < btns.length; i++) {
 		this.className += " active";
 	});
 }
+
+// fade in
+const fade = document.querySelectorAll(".fade-in");
+const slide = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+	threshold: 0,
+	rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return;
+		} else {
+			entry.target.classList.add("appear");
+			appearOnScroll.unobserve(entry.target);
+		}
+	})
+}, appearOptions);
+
+fade.forEach(fade => {
+	appearOnScroll.observe(fade);
+})
+
+slide.forEach(slider => {
+	appearOnScroll.observe(slider);
+})
